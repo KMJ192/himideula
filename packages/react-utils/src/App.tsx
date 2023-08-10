2b181399ff0221569791a39d4742b5a814c67ed4
MyeongJunKim <myeongjun222@naver.com>
2023-08-11 01:21:31 +0900

    Init Project

---
+++ packages/react-utils/src/App.tsx
@@ -0,0 +1,7 @@
+import RootRouter from './Router/RootRouter';
+
+function App() {
+  return <RootRouter />;
+}
+
+export default App;
---
+++ scripts/clearDependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm -rf .pnp.cjs .pnp.loader.mjs .yarnrc.yml .yarn yarn.lock node_modules
+rm -rf apps/documents/node_modules next-env.d.ts
