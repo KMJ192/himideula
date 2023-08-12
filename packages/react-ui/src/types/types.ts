8b73243303d698b5b7755cb147cb568b09872db4
MyeongJunKim <myeongjun222@naver.com>
2023-08-12 18:47:02 +0900

    feat: props type 설정

--- packages/react-ui/src/types/types.ts
+++ packages/react-ui/src/types/types.ts
@@ -3,10 +3,16 @@ import { ComponentPropsWithRef, ElementType } from 'react';
 type COMBINE<T, K> = T & Omit<K, keyof T>;
 
 // T가 사용자, K가 react 제공 props
-type COMBINE_ELEMENT_PROPS<T extends ElementType, K = unknown> = COMBINE<K, ComponentPropsWithRef<T>>;
+type COMBINE_ELEMENT_PROPS<T extends ElementType, K = unknown> = COMBINE<
+  K,
+  ComponentPropsWithRef<T>
+>;
 
 // 자동으로 as 타입 추가
-type OVER_RIDABLE_PROPS<T extends ElementType, K = unknown> = COMBINE_ELEMENT_PROPS<T, K> & {
+type OVER_RIDABLE_PROPS<
+  T extends ElementType,
+  K = unknown,
+> = COMBINE_ELEMENT_PROPS<T, K> & {
   as?: T;
 };
 
@@ -14,6 +20,9 @@ type RecursivePartial<T> = {
   [P in keyof T]?: RecursivePartial<T[P]>;
 };
 
-type SIZE = 'xs' | 'sm' | 'md' | 'lg';
-
-export type { OVER_RIDABLE_PROPS, COMBINE_ELEMENT_PROPS, COMBINE, SIZE, RecursivePartial };
+export type {
+  OVER_RIDABLE_PROPS,
+  COMBINE_ELEMENT_PROPS,
+  COMBINE,
+  RecursivePartial,
+};
