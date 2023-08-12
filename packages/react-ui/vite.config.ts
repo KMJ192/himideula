29787771ff65a611dcf7e4ddeb2f82011e5d1070
MyeongJunKim <myeongjun222@naver.com>
2023-08-12 18:45:19 +0900

    feat: styled 설정

--- packages/react-ui/vite.config.ts
+++ packages/react-ui/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+  ],
   resolve: {
     alias: [
       {
@@ -11,5 +17,8 @@ export default defineConfig({
         replacement: '/src',
       },
     ],
+  },
+  optimizeDeps: {
+    include: ['@emotion/react', '@emotion/styled'],
   },
 });
