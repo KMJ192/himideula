40d4af8abd1128c532831122cbd953aaee7e7d28
MyeongJunKim <myeongjun222@naver.com>
2023-08-19 20:20:16 +0900

    feat: entry Point 추가

--- packages/react-ui/src/index.ts
+++ packages/react-ui/src/index.ts
@@ -1,19 +1,29 @@
 // layouts
-export { default as Center } from '@src/components/layout/Center/Center';
-export type { CenterProps } from '@src/components/layout/Center/Center';
-export { default as Flex } from '@src/components/layout/Flex/Flex';
-export type { FlexProps } from '@src/components/layout/Flex/Flex';
-export { default as Grid } from '@src/components/layout/Grid/Grid';
-export type { GridProps } from '@src/components/layout/Grid/Grid';
-export { default as Float } from '@src/components/layout/Float/Float';
-export type { FloatProps } from '@src/components/layout/Float/Float';
-export { default as Row } from '@src/components/layout/Row/Row';
-export type { RowProps } from '@src/components/layout/Row/Row';
-export { default as Spacing } from '@src/components/layout/Spacing/Spacing';
-export type { SpacingProps } from '@src/components/layout/Spacing/Spacing';
+export { default as Center } from './components/layout/Center/Center';
+export type { CenterProps } from './components/layout/Center/Center';
+export { default as Flex } from './components/layout/Flex/Flex';
+export type { FlexProps } from './components/layout/Flex/Flex';
+export { default as Grid } from './components/layout/Grid/Grid';
+export type { GridProps } from './components/layout/Grid/Grid';
+export { default as Float } from './components/layout/Float/Float';
+export type { FloatProps } from './components/layout/Float/Float';
+export { default as Row } from './components/layout/Row/Row';
+export type { RowProps } from './components/layout/Row/Row';
+export { default as Spacing } from './components/layout/Spacing/Spacing';
+export type { SpacingProps } from './components/layout/Spacing/Spacing';
 
 // components
-export { default as Button } from '@src/components/Button/Button';
-export type { ButtonProps } from '@src/components/Button/Button';
+export { default as Button } from './components/Button/Button';
+export type { ButtonProps } from './components/Button/Button';
+export { default as Checkbox } from './components/Checkbox/Checkbox';
+export type { CheckboxProps } from './components/Checkbox/Checkbox';
+export { default as HamburgerMenu } from './components/HamburgerMenu/HamburgerMenu';
+export type { HamburgerMenuProps } from './components/HamburgerMenu/HamburgerMenu';
+export { default as Input } from './components/Input/Input';
+export type { InputProps } from './components/Input/Input';
+export { default as Spinner } from './components/Spinner/Spinner';
+export type { SpinnerProps } from './components/Spinner/Spinner';
+export { default as Text } from './components/Text/Text';
+export type { TextProps } from './components/Text/Text';
 
-export { default as Test } from '@src/components/Test/Test';
+export { default as Test } from './components/Test/Test';
