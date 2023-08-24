cb3ae806731ae4ee398771d5ae4070794747d548
MyeongJunKim <myeongjun222@naver.com>
2023-08-24 22:15:30 +0900

    feat: InfiniteScroll 컴포넌트 설정

---
+++ packages/react-modules/src/components/InfiniteScroll/InfiniteScroll.tsx
@@ -0,0 +1,74 @@
+import { useEffect, useRef, forwardRef } from 'react';
+import type { ReactNode, Ref, ElementRef } from 'react';
+
+import type { OVER_RIDABLE_PROPS } from '@src/types/types';
+
+import classNames from 'classnames/bind';
+import style from './style.module.scss';
+const cx = classNames.bind(style);
+
+type BaseProps = {
+  children?: ReactNode;
+  loadCnt?: number;
+  isLoading?: boolean;
+  loadingElement?: ReactNode;
+  hasNext?: boolean;
+  isObserve?: boolean;
+  readonly onLoad?: () => void;
+};
+
+const DEFAULT_ELEMENT = 'div';
+
+type Props<T extends React.ElementType> = OVER_RIDABLE_PROPS<T, BaseProps>;
+
+function InfiniteScroll<T extends React.ElementType = typeof DEFAULT_ELEMENT>(
+  {
+    as,
+    children,
+    loadCnt,
+    isLoading,
+    loadingElement,
+    hasNext,
+    isObserve = true,
+    onLoad,
+    className,
+    ...props
+  }: Props<T>,
+  ref: Ref<ElementRef<typeof DEFAULT_ELEMENT>>,
+) {
+  const ELEMENT = as || DEFAULT_ELEMENT;
+
+  const observingNodeRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (isObserve) {
+      const observer = new IntersectionObserver((entries) => {
+        if (Array.isArray(entries) && entries[0].isIntersecting) {
+          if (onLoad) onLoad();
+        }
+      });
+
+      if (observingNodeRef.current) {
+        observer.observe(observingNodeRef.current);
+      }
+
+      return () => {
+        observer.disconnect();
+      };
+    }
+    return () => {};
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [loadCnt, isObserve]);
+
+  return (
+    <ELEMENT {...props} ref={ref} className={cx('container', className)}>
+      {children}
+      <div ref={observingNodeRef} className={cx('loader', { hasNext })}>
+        {isLoading && loadingElement}
+      </div>
+    </ELEMENT>
+  );
+}
+
+export type { Props as InfiniteScrollProps };
+export default forwardRef(InfiniteScroll) as typeof InfiniteScroll;
--- packages/react-modules/src/index.ts
+++ packages/react-modules/src/index.ts
@@ -3,3 +3,6 @@ export type { RouterMap } from './components/Router/types';
 
 export { Switch, Case, Default } from './components/IfComponents/SwitchCase';
 export { When, Unless } from './components/IfComponents/WhenUnless';
+
+export { default as InfiniteScroll } from './components/InfiniteScroll/InfiniteScroll';
+export type { InfiniteScrollProps } from './components/InfiniteScroll/InfiniteScroll';
