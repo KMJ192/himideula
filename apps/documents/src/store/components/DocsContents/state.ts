696947c0474fb1efdefca6a800a067eb35f4d581
MyeongJunKim <myeongjun222@naver.com>
2023-08-28 20:50:48 +0900

    feat(docs): Docs 컨텐츠, state 추가

---
+++ apps/documents/src/store/components/DocsContents/state.ts
@@ -0,0 +1,43 @@
+import React from 'react';
+import { create } from 'zustand';
+
+type Documents = {
+  title: string;
+  description: string;
+  view: React.ReactNode;
+  componentString: string;
+};
+
+type APIs = {
+  // ...
+};
+type CSSVars = {
+  // ...
+};
+type Playground = {
+  // ...
+};
+
+type State = {
+  title: string;
+  documents: Array<Documents>;
+  apis: Array<APIs>;
+  cssVars: Array<CSSVars>;
+  playground: Array<Playground>;
+};
+
+type Action = {
+  viewComponent: (state: State) => void;
+};
+
+const useDocsContentsState = create<State & Action>((set) => ({
+  title: '',
+  documents: [],
+  apis: [],
+  cssVars: [],
+  playground: [],
+  viewComponent: (newState: State) => set(newState),
+}));
+
+export type { Documents, APIs, CSSVars, Playground };
+export { useDocsContentsState };
