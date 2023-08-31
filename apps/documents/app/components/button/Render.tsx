92b60d6332abfee7fcb6e9878e8133b786e92283
MyeongJunKim <myeongjun222@naver.com>
2023-08-31 20:25:00 +0900

    feat(docs): Button docs api 출력 추가

--- apps/documents/app/components/button/Render.tsx
+++ apps/documents/app/components/button/Render.tsx
@@ -1,8 +1,9 @@
 import { Button } from '@ssamssam/react-ui';
 
 import DocsContents from '@src/components/DocsContents/DocsContents';
+import type { APIs, Documents } from '@src/store/components/DocsContents/state';
 
-const documents = [
+const documents: Array<Documents> = [
   {
     title: 'Basic',
     description: '기본 버튼 컴포넌트 입니다.',
@@ -29,12 +30,58 @@ const documents = [
   },
 ];
 
+const apis: Array<APIs> = [
+  {
+    name: 'children',
+    type: 'React.ReactNode',
+    defaultValue: 'undefined',
+    description: 'Children 컴포넌트',
+  },
+  {
+    name: 'variant',
+    type: `"primary" | "outlined" | "clear"`,
+    defaultValue: `"primary"`,
+    description: 'Button 컴포넌트 변환 타입',
+  },
+  {
+    name: 'shape',
+    type: `"rect" | "circle" | "square"`,
+    defaultValue: 'rect',
+    description: 'Button 컴포넌트 모양',
+  },
+  {
+    name: 'loading',
+    type: 'boolean',
+    defaultValue: 'false',
+    description: '로딩 버튼 여부',
+  },
+  {
+    name: 'loadingElement',
+    type: 'React.ReactNode',
+    defaultValue: 'undefined',
+    description: '로딩 컴포넌트 주입',
+  },
+  {
+    name: 'leftIcon',
+    type: 'React.ReactNode',
+    defaultValue: 'undefined',
+    description: '좌측 아이콘',
+  },
+  {
+    name: 'rightIcon',
+    type: 'React.ReactNode',
+    defaultValue: 'undefined',
+    description: '우측 아이콘',
+  },
+];
+
 function Render() {
   return (
     <DocsContents
       title='Button'
       description='Button 컴포넌트'
       documents={documents}
+      apis={apis}
     />
   );
 }
