53d90503303288a4655f20c6e944cf466726d636
MyeongJunKim <myeongjun222@naver.com>
2023-08-31 15:56:51 +0900

    feat(docs): DocsContents 스타일 수정

--- apps/documents/src/components/DocsContents/Documents/Documents.tsx
+++ apps/documents/src/components/DocsContents/Documents/Documents.tsx
@@ -20,7 +20,7 @@ function Documents() {
                 <Text typo='h3'>{title}</Text>
                 <Text typo='title2'>{description}</Text>
                 <div className={cx('view')}>{view}</div>
-                <div className={cx('guide')}>{componentString}</div>
+                <div className={cx('code-guide')}>{componentString}</div>
               </Flex>
               <Spacing direction='vertical' spacing={48} />
             </Fragment>
