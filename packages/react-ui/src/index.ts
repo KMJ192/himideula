9b7536e8f5adf2f88475674550c37f79aa79f15c
MyeongJunKim <myeongjun222@naver.com>
2023-09-06 02:54:20 +0900

    feat(react-ui): Badge 컴포넌트 개발 (fp)

--- packages/react-ui/src/index.ts
+++ packages/react-ui/src/index.ts
@@ -15,6 +15,8 @@ export { default as Stack } from './layout/Stack/Stack';
 export type { StackProps } from './layout/Stack/Stack';
 
 // components
+export { default as Badge } from './components/Badge/Badge';
+export type { BadgeProps } from './components/Badge/Badge';
 export { default as Button } from './components/Button/Button';
 export type { ButtonProps } from './components/Button/Button';
 export { default as Card } from './components/Card/Card';
