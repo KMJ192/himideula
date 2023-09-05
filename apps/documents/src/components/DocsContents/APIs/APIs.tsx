5c1375d79cc8b9c9c16e9b86ea2a7bb9e0d4cd6d
MyeongJunKim <myeongjun222@naver.com>
2023-09-06 02:56:59 +0900

    feat(documents): 문서 페이지 스타일 수정

--- apps/documents/src/components/DocsContents/APIs/APIs.tsx
+++ apps/documents/src/components/DocsContents/APIs/APIs.tsx
@@ -1,4 +1,4 @@
-import { Table, Text } from '@ssamssam/react-ui';
+import { Badge, Table, Text } from '@ssamssam/react-ui';
 
 import { useDocsContentsState } from '@src/store/components/DocsContents/state';
 
@@ -12,20 +12,38 @@ function APIs() {
       </Table.Caption>
       <Table.Thead>
         <Table.Tr>
-          <Table.Th>Name</Table.Th>
-          <Table.Th>Type</Table.Th>
-          <Table.Th>Default value</Table.Th>
-          <Table.Th>Description</Table.Th>
+          <Table.Th>
+            <Text typo='t2'>Name</Text>
+          </Table.Th>
+          <Table.Th>
+            <Text typo='t2'>Type</Text>
+          </Table.Th>
+          <Table.Th>
+            <Text typo='t2'>Default value</Text>
+          </Table.Th>
+          <Table.Th>
+            <Text typo='t2'>Description</Text>
+          </Table.Th>
         </Table.Tr>
       </Table.Thead>
       <Table.Tbody>
         {apis.map(({ name, type, description, defaultValue }, index) => {
           return (
             <Table.Tr key={`${index}-${name}`}>
-              <Table.Td>{name}</Table.Td>
-              <Table.Td>{type}</Table.Td>
-              <Table.Td>{defaultValue}</Table.Td>
-              <Table.Td>{description}</Table.Td>
+              <Table.Td>
+                <Text typo='s1'>{name}</Text>
+              </Table.Td>
+              <Table.Td>
+                <Text typo='s2'>{type}</Text>
+              </Table.Td>
+              <Table.Td>
+                <Badge colorSchema='info'>
+                  <Text typo='c1'>{defaultValue}</Text>
+                </Badge>
+              </Table.Td>
+              <Table.Td>
+                <Text typo='s2'>{description}</Text>
+              </Table.Td>
             </Table.Tr>
           );
         })}
--- apps/documents/src/components/DocsContents/CSSVariable/CSSVariable.tsx
+++ apps/documents/src/components/DocsContents/CSSVariable/CSSVariable.tsx
@@ -12,18 +12,30 @@ function CSSVariable() {
       </Table.Caption>
       <Table.Thead>
         <Table.Tr>
-          <Table.Th>Name</Table.Th>
-          <Table.Th>Type</Table.Th>
-          <Table.Th>Default value</Table.Th>
-          <Table.Th>Description</Table.Th>
+          <Table.Th>
+            <Text typo='t2'>Name</Text>
+          </Table.Th>
+          <Table.Th>
+            <Text typo='t2'>Type</Text>
+          </Table.Th>
+          <Table.Th>
+            <Text typo='t2'>Default value</Text>
+          </Table.Th>
+          <Table.Th>
+            <Text typo='t2'>Description</Text>
+          </Table.Th>
         </Table.Tr>
       </Table.Thead>
       <Table.Tbody>
         {cssVar.map(({ name, type, defaultValue, description }, index) => {
           return (
             <Table.Tr key={`${index}-${name}`}>
-              <Table.Td>{name}</Table.Td>
-              <Table.Td>{type}</Table.Td>
+              <Table.Td>
+                <Text typo='s1'>{name}</Text>
+              </Table.Td>
+              <Table.Td>
+                <Text typo='s2'>{type}</Text>
+              </Table.Td>
               <Table.Td>
                 <Flex
                   style={{
@@ -39,10 +51,12 @@ function CSSVariable() {
                       boxShadow: '0px 3px 6px rgba(0, 0, 0, .2)',
                     }}
                   ></div>
-                  <div>{defaultValue}</div>
+                  <Text typo='c1'>{defaultValue}</Text>
                 </Flex>
               </Table.Td>
-              <Table.Td>{description}</Table.Td>
+              <Table.Td>
+                <Text typo='s2'>{description}</Text>
+              </Table.Td>
             </Table.Tr>
           );
         })}
