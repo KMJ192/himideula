65e5ca8f01b4a0cd0a0eefb4a09b1ad78a3f8a10
MyeongJunKim <myeongjun222@naver.com>
2023-09-06 23:04:36 +0900

    feat(documents): Button문서 페이지 설명 문구 수정 (fp)

--- apps/documents/src/pageContents/components/Button/ButtonDocs.tsx
+++ apps/documents/src/pageContents/components/Button/ButtonDocs.tsx
@@ -1,15 +1,15 @@
 import DocsContents from '@src/components/ComponentsDocsContents/DocsContents';
 
+import Playground from './Playground/Playground';
 import { documents } from './documents/documents';
 import { apis } from './apis/apis';
 import { cssVar } from './cssVar/cssVar';
-import Playground from './Playground/Playground';
 
 function ButtonDocs() {
   return (
     <DocsContents
       title='Button'
-      description='Button 컴포넌트'
+      description='Button 컴포넌트 가이드'
       documents={documents}
       apis={apis}
       cssVar={cssVar}
