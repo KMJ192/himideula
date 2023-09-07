510665ba367067d5b36cad155843b2145ee37dcb
MyeongJunKim <myeongjun222@naver.com>
2023-09-07 15:50:55 +0900

    feat(documents): 문서페이지 내용 수정 (fp)

--- apps/documents/src/pageContents/components/Badge/cssVar/cssVar.ts
+++ apps/documents/src/pageContents/components/Badge/cssVar/cssVar.ts
@@ -6,13 +6,13 @@ const cssVar: Array<CSSVar> = [
     name: '--ss-c-badge-color',
     type: 'Color',
     defaultValue: COLOR.LIGHT.primary100,
-    description: '문자색상',
+    description: '문자 색상',
   },
   {
     name: '--ss-c-badge-bg',
     type: 'Color',
     defaultValue: COLOR.LIGHT.primary700,
-    description: '배경색상',
+    description: '배경 색상',
   },
 ];
 export { cssVar };
