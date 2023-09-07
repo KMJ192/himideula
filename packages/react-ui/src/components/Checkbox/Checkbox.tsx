49148dd9c1d5612f81ebecbec65ea09613a37151
MyeongJunKim <myeongjun222@naver.com>
2023-09-08 01:40:05 +0900

    feat(react-ui): 컴포넌트 기능 축소 (fp)

--- packages/react-ui/src/components/Checkbox/Checkbox.tsx
+++ packages/react-ui/src/components/Checkbox/Checkbox.tsx
@@ -5,8 +5,6 @@ import Mark from './Mark';
 
 import type { OVER_RIDABLE_PROPS } from '@src/types/types';
 
-import { getStyle } from './calcStyle';
-
 import classNames from 'classnames/bind';
 import style from './style.module.scss';
 const cx = classNames.bind(style);
@@ -16,7 +14,6 @@ type BaseProps = {
   checked?: boolean;
   multiple?: boolean;
   disabled?: boolean;
-  size?: number;
 };
 
 const DEFAULT_ELEMENT = 'div';
@@ -30,17 +27,12 @@ function Checkbox<T extends React.ElementType = typeof DEFAULT_ELEMENT>(
     checked = false,
     multiple = false,
     disabled = false,
-    size,
     className,
     ...props
   }: Props<T>,
   ref: React.Ref<any>,
 ) {
   const ELEMENT = as || DEFAULT_ELEMENT;
-
-  const curStyle = getStyle({
-    size,
-  });
 
   return (
     <Flex
@@ -54,8 +46,8 @@ function Checkbox<T extends React.ElementType = typeof DEFAULT_ELEMENT>(
         className,
       )}
     >
-      <div style={curStyle} className={cx('box', { checked }, { disabled })}>
-        <Mark multiple={multiple} size={size} />
+      <div className={cx('box', { checked }, { disabled })}>
+        <Mark multiple={multiple} />
       </div>
       <span className={cx('children')}>{children}</span>
     </Flex>
